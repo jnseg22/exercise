158001bcd9e4f3496528477be67322dfd41ec9d1
junsung <jnseg22@gmail.com>
2019-06-22 15:10:09 +0900

    댓글과 댓글 수에 대한 처리(댓글 등록, 삭제시 트랜잭션)

--- Board1/src/main/java/com/home/service/ReplyServiceImpl.java
+++ Board1/src/main/java/com/home/service/ReplyServiceImpl.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.home.domain.Criteria;
 import com.home.domain.ReplyPageDTO;
 import com.home.domain.ReplyVO;
+import com.home.mapper.BoardMapper;
 import com.home.mapper.ReplyMapper;
 
 import lombok.Setter;
@@ -20,10 +22,16 @@ public class ReplyServiceImpl implements ReplyService{
 	@Setter(onMethod_ = @Autowired)
 	private ReplyMapper mapper;
 	
+	@Setter(onMethod_ = @Autowired)
+	private BoardMapper boardMapper;
+	
+	@Transactional
 	@Override
 	public int register(ReplyVO vo) {
 		
 		log.info("register........" + vo);
+		
+		boardMapper.updateReplyCnt(vo.getBno(), 1);
 		
 		return mapper.insert(vo);
 	}
@@ -44,10 +52,15 @@ public class ReplyServiceImpl implements ReplyService{
 		return mapper.update(vo);
 	}
 	
+	@Transactional
 	@Override
 	public int remove(Long rno) {
 		
 		log.info("remove..........." + rno);
+		
+		ReplyVO vo = mapper.read(rno);
+		
+		boardMapper.updateReplyCnt(vo.getBno(), -1);
 		
 		return mapper.delete(rno);
 	}
